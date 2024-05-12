1ebfd54595b72d6efb704a5d91ca872706304124
AMIDU ASAAH TEKUH <151391308+TEKUH01@users.noreply.github.com>
2024-05-12 14:51:02 -0400

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,9 @@
+package com.example.springbootmongodemo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    // Define your service class here
+}
