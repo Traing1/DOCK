ebf6b0726f015324473bedb5b515a2b9df068562
AMIDU ASAAH TEKUH <151391308+TEKUH01@users.noreply.github.com>
2024-05-12 14:38:04 -0400

    Create MainServiceTest.java

---
+++ MainServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.springbootmongodemo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainServiceTest {
+
+    @Test
+    public void testExample() {
+        // Write your test logic here
+        assertEquals(2, 1 + 1);
+    }
+}
